f08ce4069c2c5e1f29552b461c50aeff305c154a
chengguangzhuang <1223445257@qq.com>
2022-04-22 18:40:39 +0800

    feat: server init

--- bin/npm-ui.js
+++ bin/npm-ui.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 console.log('main')
+
+
+
+const createServer = require('../lib/server.js')
+createServer()
+console.log(createServer,"--cer")
--- src/views/demo.vue
+++ src/views/demo.vue
@@ -1,9 +1,26 @@
 <template>
 <div class="demo">demo for webpack-dev-server</div>
+
 </template>
 <script>
 export default {
-  name: 'demo'
+  name: 'demo',
+
+  mounted() {
+    const ws = new WebSocket('ws://localhost:3000')
+    ws.onopen = function(evt) {
+      console.log('Connection open ...')
+
+      ws.send(JSON.stringify({
+        type: 'init',
+        content: 'form client init'
+      }))
+
+      ws.onmessage = function(event) {
+        console.log(JSON.parse(event))
+      }
+    }
+  },
 }
 </script>
 <style lang="scss">
